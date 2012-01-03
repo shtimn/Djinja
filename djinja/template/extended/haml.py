ab57513ec64b4ec8dd1bcc589178e5e250156386
Alexey Nikitin <shtimn@gmail.com>
2012-01-03 15:41:27 +0400

    Removed redundancy in HAML extension.

--- djinja/template/extended/haml.py
+++ djinja/template/extended/haml.py
@@ -1,54 +1,36 @@
-from hamlpy.nodes import *
+from hamlpy.nodes import TagNode, RootNode, TAG
+from hamlpy.nodes import create_node as _create_node
 from hamlpy import hamlpy
+
+
 def create_node(haml_line):
     stripped_line = haml_line.strip()
-    
-    if not stripped_line:
-        return None
-        
-    if stripped_line[0] == HAML_ESCAPE:
-        return HamlNode(haml_line.replace(HAML_ESCAPE, '', 1))
-        
-    if stripped_line[0] in ELEMENT_CHARACTERS:
-        return ElementNode(haml_line)
-    
-    if stripped_line[0] == HTML_COMMENT:
-        return CommentNode(haml_line)
-    
-    if stripped_line.startswith(HAML_COMMENT):
-        return HamlCommentNode(haml_line)
-    
-    if stripped_line[0] == VARIABLE:
-        return VariableNode(haml_line)
-    
+
     if stripped_line[0] == TAG:
         return JinjaTagNode(haml_line)
-    
-    if stripped_line == JAVASCRIPT_FILTER:
-        return JavascriptFilterNode(haml_line)
-    
-    if stripped_line == CSS_FILTER:
-        return CssFilterNode(haml_line)
-    
-    if stripped_line == PLAIN_FILTER:
-        return PlainFilterNode(haml_line)
-    
-    return HamlNode(haml_line)
+
+    return _create_node(haml_line)
+
 
 class JinjaTagNode(TagNode):
-    self_closing = {'for':'endfor',
-                    'if':'endif',
-                    'block':'endblock',
-                    'filter':'endfilter',
-                    'autoescape':'endautoescape',
-                    'with':'endwith',
-                    'macro':'endmacro',
-                    'call':'endcall',
-                    'trans':'endtrans'
-    }
-    may_contain = {'if':['else','elif'], 'for':['empty'], 'with':['with'],'trans':['pluralize']}
+    self_closing = TagNode.self_closing
+    self_closing.update({
+        'macro': 'endmacro',
+        'call': 'endcall',
+        'trans': 'endtrans'
+    })
+
+    may_contain = dict((key, [val]) for key, val in TagNode.may_contain.iteritems())
+    may_contain.update({
+        'if': ['else', 'elif'],
+        'for': ['empty'],
+        'with': ['with'],
+        'trans': ['pluralize']
+    })
+
     def should_contain(self, node):
-        return isinstance(node,TagNode) and node.tag_name in self.may_contain.get(self.tag_name,[])
+        return isinstance(node, TagNode) and node.tag_name in self.may_contain.get(self.tag_name, [])
+
 
 class Compiler(hamlpy.Compiler):
     def process_lines(self, haml_lines):
